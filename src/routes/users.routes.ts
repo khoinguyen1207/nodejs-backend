fa0f261267b6a0c14a7fc837ad3e6adcb6a6b340
Khoi Nguyen <khoinguyentpvl@gmail.com>
2024-12-11 00:05:45 +0700

    refactor: update config file

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express"
 const usersRouter = Router()
 import { loginController, registerController } from "~/controllers/users.controllers"
 import { loginValidator, registerValidator } from "~/middlewares/users.middlewares"
-import { wrapRequestHandler } from "~/utils/errorHandler"
+import { wrapRequestHandler } from "~/utils/error-handler"
 
 usersRouter.post("/login", loginValidator, loginController)
 usersRouter.post("/register", registerValidator, wrapRequestHandler(registerController))
