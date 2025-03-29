0d2bec5d12a8b933db0c3e9f2bd35000d43aee5a
Khoi Nguyen <khoinguyentpvl@gmail.com>
2025-03-29 19:21:30 +0700

    feat: handle arguments via command with minimist package

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,10 +1,18 @@
 import dotenv from "dotenv"
+import argv from "minimist"
 dotenv.config()
+
+const commandArgs = argv(process.argv.slice(2))
+
+export const isEnvProduction = () => {
+  return Boolean(commandArgs.production)
+}
 
 export const envConfig = {
   DB_USERNAME: process.env.DB_USERNAME,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
+  PORT: process.env.PORT || 4000,
 
   PASSWORD_SECRET_KEY: process.env.PASSWORD_SECRET_KEY,
   ACCESS_TOKEN_SECRET_KEY: process.env.ACCESS_TOKEN_SECRET_KEY || "",
