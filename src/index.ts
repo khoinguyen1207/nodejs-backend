70b20e9603fe055b5a9ba53f142c8277311cc052
nkhoinguyen <khoinguyentpvl@gmail.com>
2025-08-17 19:09:21 +0700

    feat: add rate limiting

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ import searchRouter from "~/routes/searchs.routes"
 
 import databaseService from "~/services/database.services"
 import logRequest from "~/middlewares/logging.middlewares"
+import rateLimit from "express-rate-limit"
 
 const app = express()
 const port = envConfig.PORT
@@ -30,6 +31,7 @@ databaseService.connect().then(() => {
   databaseService.indexTweet()
 })
 app.use(express.json())
+app.use(logRequest)
 app.use(helmet())
 app.use(
   cors({
@@ -37,8 +39,15 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),
 )
-app.use(logRequest)
-
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 100, // Limit each IP to 100 requests per windowMs
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  }),
+)
 // Routes
 app.use("/users", usersRouter)
 app.use("/auths", authsRouter)
